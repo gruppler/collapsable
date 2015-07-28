3e48eae675d83ce155ebeb8ac45765f68d2ca5a5
U-Gioco\gruppler <gruppler@gioco.(none)>
2015-07-28 01:04:26 -0400

    Initial commit

---
+++ collapsable.jquery.js
@@ -0,0 +1,53 @@
+/* Copyright 2015 Craig Laparo */
+
+'use strict';
+
+(function($){
+  $.fn.collapsable = function(options){
+    var config = $.extend({
+      maxHeight: '10em',
+      transitions: true
+    }, options);
+
+    var _show = function(){
+      $(this).removeClass('collapsed').css('max-height', $(this).children().height());
+    };
+
+    var _hide = function(){
+      $(this).addClass('collapsed').css('max-height', config.maxHeight);
+    };
+
+    var _toggle = function(){
+      var $this = $(this);
+
+      if($this.hasClass('collapsed')){
+        _show.call(this);
+      }else{
+        _hide.call(this);
+      }
+    };
+
+    this.each(function(){
+      var $this = $(this)
+        , full_height = $this.height();
+
+      if($this.data('collapsable')){
+        return;
+      }
+
+      $this.data('collapsable', true).addClass('collapsable');
+
+      $this.css('max-height', config.maxHeight);
+      if($this.height() < full_height){
+        if(!config.transitions){
+          $this.addClass('no-transitions');
+        }
+        $this.css('max-height', config.maxHeight).addClass('collapsed').click(_toggle);
+      }else{
+        $this.css('max-height', '');
+      }
+    });
+
+    return this;
+  };
+})(jQuery);
